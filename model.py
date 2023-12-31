e0a8af85dcb05043d5107fa5390cfa0221e5e796
omid-sar <mr.omid.sardari@gmail.com>
2023-12-31 16:47:18 -0500

    initial

---
+++ model.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+
+
+embedding_layer = nn.Embedding(num_embeddings =100, embedding_dim=512)
+sd = torch.tensor(1) 
+torch.tensor(15)
+torch.tensor([13], dtype=torch.long)
+embedding_layer
