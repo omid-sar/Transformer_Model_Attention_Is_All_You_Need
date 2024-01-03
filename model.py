ea9c0e14a6f25350fa81daa5ba19f1b98ebdbfd0
omid-sar <mr.omid.sardari@gmail.com>
2024-01-02 19:31:57 -0500

    positional_encoddings

--- model.py
+++ model.py
@@ -52,20 +52,25 @@ class PositionalEncodding(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
     # Create a matrix of shape(seq_len, d_model)
-    pe = torch.zeros(seq_len, d_model)
-    # Create poition vector size (seq_len, 1)
-    position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
-    # Calculate positional encoding to EVEN and ODD columns 
-    div_term = torch.exp(torch.arange(0, d_model, 2).float()* (- math.log(10000.0)/ d_model))
-    pe[:, 0::2] = torch.sin(position * div_term)
-    pe[:, 1::2] = torch.cos(position * div_term)
+        pe = torch.zeros(seq_len, d_model)
+        # Create poition vector size (seq_len, 1)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        # Calculate positional encoding to EVEN and ODD columns 
+        div_term = torch.exp(torch.arange(0, d_model, 2).float()* (- math.log(10000.0)/ d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
 
-    # Add batch dimension to tensor (seq_len, d_model) -> (1, seq_len, d_model)
-    pe = pe.unsqueeze(0)
+        # Add batch dimension to tensor (seq_len, d_model) -> (1, seq_len, d_model)
+        pe = pe.unsqueeze(0)
 
-    # Register "pe" in the buffer of this module:It ensures that the buffer's state is included 
-    # when you save (torch.save) and load (torch.load) the model
-    self.register_buffer('pe', pe)
+        # Register "pe" in the buffer of this module:It ensures that the buffer's state is included 
+        # when you save (torch.save) and load (torch.load) the model
+        # Also we wont learn them , we calcualte once and then just we used it
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + pe[:,x.shap[1],:].requires_grad(False)
+        return self.dropout(x)
 
 
 
