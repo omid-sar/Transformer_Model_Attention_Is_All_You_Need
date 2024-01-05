b3bacee4831a2f04b4c76628300b67773ae86667
omid-sar <mr.omid.sardari@gmail.com>
2024-01-05 10:28:42 -0500

    initial

--- model.py
+++ model.py
@@ -85,10 +85,31 @@ class FeedForwardBlock(nn.Module):
 
     def forward(self, x):
         out = self.linear_1(x)
-        out = torch.nn.functional.relu(out)
+        out = nn.functional.relu(out)
         out = self.dropout(out)
         out = self.linear_2(out)
         return out
+
+
+
+class MultiHeadAttentionBlock(nn.Module):
+
+    def __init__(self,d_model: int, h: int, dropout: float) -> None:
+        
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        assert d_model % h == 0, "d_model is not divisble by h"
+
+        self.d_k = d_model // h
+        self.w_q = nn.Linear(d_model, d_model) # Wq
+        self.w_k = nn.Linear(d_model, d_model) # Wk
+        self.w_v = nn.Linear(d_model, d_model) # Wv
+
+        self.w_o = nn.Linear(d_model, d_model) # Wo
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
 
 
 
