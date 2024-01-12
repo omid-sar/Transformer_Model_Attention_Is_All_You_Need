8f961c9ac2a023b64b3a7be6926865ae9c7adcc1
omid-sar <mr.omid.sardari@gmail.com>
2024-01-12 18:11:47 -0500

    modify_requirements

--- train.py
+++ train.py
@@ -2,16 +2,33 @@ import torch
 import torch.nn as nn
 
 # Huggingface Tokenizers
+from datasets import load_dataset
 from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
 from tokenizers.trainers import WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace
 
 from pathlib import Path
+
+
+# ----------------------------------------------------------------------------------------------------
+# my cheat sheets 
+config = {'tokenizer_file': '../data/raw/tokenizer_file_{}.json', 'datasource': 'tep_en_fa_para',
+          'lang_src': 'en', 'lang_tgt': 'fa'
+           }
+# ----------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------
+# help me to realize the Path Module
 """config = {'tokenizer_file': '../data/raw/tokenizer_file_{}.json'}
 lang = 'en'
 tokenizer_path = Path(config['tokenizer_file'].format(lang))"""
 
+
+# ----------------------------------------------------------------------------------------------------
+def get_all_sentences(ds, lang):
+    for item in ds:
+        yield item['translation'][lang]
 
 def get_or_build_tokenizer(config, ds, lang):
     tokenizer_path = Path(config['tokenizer_file'].format(lang))
@@ -21,9 +38,81 @@ def get_or_build_tokenizer(config, ds, lang):
         tokenizer.pre_tokenizer = Whitespace()
         trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2)
         tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer=trainer)
+        # added line of code 
+        tokenizer_path.parent.mkdir(parents=True, exist_ok=True)
         tokenizer.save(str(tokenizer_path))
     else:
         tokenizer = Tokenizer.from_file(str(tokenizer_path))
     return tokenizer
+
+# ----------------------------------------------------------------------------------------------------
+# the differnce between "opus_books" dataset and "tep_en_fa_para"
+"""
+load_dataset('tep_en_fa_para')
+
+DatasetDict({
+    train: Dataset({
+        features: ['translation'],
+        num_rows: 612087
+    })
+})
+load_dataset('opus_books', 'en-it')
+
+DatasetDict({
+    train: Dataset({
+        features: ['id', 'translation'],
+        num_rows: 32332
+    })
+})
+"""
+
+f"{config['datasource']}"
+
+# ----------------------------------------------------------------------------------------------------
+
+
+ds_raw = load_dataset(f"{config['datasource']}", split='train')
+ds_raw
+# Build Tokenoizers
+tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
+tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, config['lang_tgt'])
+
+
+
+def get_ds(config):
+    # It only has the train split, so we divide it overselves
+    ds_raw = load_dataset(f"{config['datasource']}", f"{config['lang_src']}-{config['lang_tgt']}", split='train')
+
+    # Build tokenizers
+    tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
+    tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, config['lang_tgt'])
+
+    # Keep 90% for training, 10% for validation
+    train_ds_size = int(0.9 * len(ds_raw))
+    val_ds_size = len(ds_raw) - train_ds_size
+    train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
+
+    train_ds = BilingualDataset(train_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+    val_ds = BilingualDataset(val_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+
+    # Find the maximum length of each sentence in the source and target sentence
+    max_len_src = 0
+    max_len_tgt = 0
+
+    for item in ds_raw:
+        src_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
+        tgt_ids = tokenizer_tgt.encode(item['translation'][config['lang_tgt']]).ids
+        max_len_src = max(max_len_src, len(src_ids))
+        max_len_tgt = max(max_len_tgt, len(tgt_ids))
+
+    print(f'Max length of source sentence: {max_len_src}')
+    print(f'Max length of target sentence: {max_len_tgt}')
+    
+
+    train_dataloader = DataLoader(train_ds, batch_size=config['batch_size'], shuffle=True)
+    val_dataloader = DataLoader(val_ds, batch_size=1, shuffle=True)
+
+    return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
+
 
 
