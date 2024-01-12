924be00e57a4b67dc55fc97318cf08bc41a62e21
omid-sar <mr.omid.sardari@gmail.com>
2024-01-12 16:55:54 -0500

    get_or_build_tokenizer

--- train.py
+++ train.py
@@ -8,19 +8,22 @@ from tokenizers.trainers import WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace
 
 from pathlib import Path
-config = {'tokenizer_file': '../data/raw/tokenizer_file_{}.json'}
+"""config = {'tokenizer_file': '../data/raw/tokenizer_file_{}.json'}
 lang = 'en'
-tokenizer_path = Path(config['tokenizer_file'].format(lang))
+tokenizer_path = Path(config['tokenizer_file'].format(lang))"""
 
-print(x.absolute())
+
 def get_or_build_tokenizer(config, ds, lang):
     tokenizer_path = Path(config['tokenizer_file'].format(lang))
+    if not tokenizer_path.exists():
+        # Huggigface code
+        tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
+        tokenizer.pre_tokenizer = Whitespace()
+        trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2)
+        tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer=trainer)
+        tokenizer.save(str(tokenizer_path))
+    else:
+        tokenizer = Tokenizer.from_file(str(tokenizer_path))
+    return tokenizer
 
 
-Path.exists(tokenizer_path)
-tokenizer_path.exists()
-Tokenizer()
-WordLevel()
-
-tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
-tokenizer.pre_tokenizer()
