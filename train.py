2bc9911ff286b0514cff7d2464992c41adb2d5e9
omid-sar <mr.omid.sardari@gmail.com>
2024-01-12 18:42:02 -0500

    get_ds_tokenizers

--- train.py
+++ train.py
@@ -1,5 +1,11 @@
+# Local Modules
+from dataset import BilingualDataset
+
+
 import torch
 import torch.nn as nn
+from torch.utils.data import random_split
+from torch.utils.data import DataLoader
 
 # Huggingface Tokenizers
 from datasets import load_dataset
@@ -14,7 +20,7 @@ from pathlib import Path
 # ----------------------------------------------------------------------------------------------------
 # my cheat sheets 
 config = {'tokenizer_file': '../data/raw/tokenizer_file_{}.json', 'datasource': 'tep_en_fa_para',
-          'lang_src': 'en', 'lang_tgt': 'fa'
+          'lang_src': 'en', 'lang_tgt': 'fa', 'seq_len': 350, 'batch_size':8
            }
 # ----------------------------------------------------------------------------------------------------
 
@@ -50,6 +56,10 @@ def get_or_build_tokenizer(config, ds, lang):
 """
 load_dataset('tep_en_fa_para')
 
+Max length of source sentence: 37
+Max length of target sentence: 37
+
+
 DatasetDict({
     train: Dataset({
         features: ['translation'],
@@ -72,11 +82,33 @@ f"{config['datasource']}"
 
 
 ds_raw = load_dataset(f"{config['datasource']}", split='train')
-ds_raw
 # Build Tokenoizers
 tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
 tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, config['lang_tgt'])
+# 90% training, 10% validation
+train_ds_size = int(0.9 * len(ds_raw))
+val_ds_size = len(ds_raw) - train_ds_size
+train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
 
+train_ds = BilingualDataset(train_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+val_ds = BilingualDataset(val_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+
+# Find the maximum length of each sentence in the source and target sentence
+max_len_src = 0
+max_len_tgt = 0
+
+for item in ds_raw:
+    src_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
+    tgt_ids = tokenizer_tgt.encode(item['translation'][config['lang_tgt']]).ids
+    max_len_src = max(max_len_src, len(src_ids))
+    max_len_tgt = max(max_len_tgt, len(tgt_ids))
+
+print(f'Max length of source sentence: {max_len_src}')
+print(f'Max length of target sentence: {max_len_tgt}')
+
+
+train_dataloader = DataLoader(train_ds, batch_size=config['batch_size'], shuffle=True)
+val_dataloader = DataLoader(val_ds, batch_size=1, shuffle=True)
 
 
 def get_ds(config):
