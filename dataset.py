c5e83b67cf5952c74316712632ebd38ccbbc64f8
omid-sar <mr.omid.sardari@gmail.com>
2024-01-16 18:02:48 -0500

    get_ds

--- dataset.py
+++ dataset.py
@@ -87,8 +87,9 @@ class BilingualDataset(Dataset):
             "tgt_text": tgt_text,
         }
 
-     
-    def causal_mask(size):
-        mask = torch.triu(torch.ones(1, size, size), diagonal=1).type(torch.int)
-        return mask == 0
+    
 
+
+def causal_mask(size):
+    mask = torch.triu(torch.ones((1, size, size)), diagonal=1).type(torch.int)
+    return mask == 0
--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 # Local Modules
-from dataset import BilingualDataset
+from dataset import BilingualDataset, causal_mask
 
 
 import torch
@@ -101,6 +101,45 @@ DatasetDict({
 # ----------------------------------------------------------------------------------------------------
 
 
+def get_ds(config):
+    # Dataset only has a train split, so we divide it
+    ds_raw = load_dataset(f"{config['datasource']}", split='train', )
+
+    # Build Tokenoizers
+    tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
+    tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, config['lang_tgt'])
+
+    # 90% training, 10% validation
+    train_ds_size = int(0.9 * len(ds_raw))
+    val_ds_size = len(ds_raw) - train_ds_size
+    train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
+
+
+    # Convert train and validate dataset to tokens by padding, mask, SOS, EOS and etc.
+    train_ds = BilingualDataset(train_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+    val_ds = BilingualDataset(val_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+
+    # Find the maximum length of each sentence in the source and target sentence
+    max_len_src = 0
+    max_len_tgt = 0
+
+    for item in ds_raw:
+       src_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
+       tgt_ids = tokenizer_src.encode(item['translation'][config['lang_tgt']]).ids
+       max_len_src = max(len(src_ids), max_len_src)
+       max_len_tgt = max(len(tgt_ids), max_len_tgt)
+
+    print(f'Max length of source sentence: {max_len_src}')
+    print(f'Max length of target sentence: {max_len_tgt}')
+
+    train_dataloader = DataLoader(train_ds, batch_size=config['batch_size'], shuffle=True)
+    val_dataloader = DataLoader(val_ds, batch_size=1, shuffle=True)
+
+    return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
+
+----------------------------------------------------------------------------------------------------   
+
+""""
 ds_raw = load_dataset(f"{config['datasource']}", split='train', )
 # Build Tokenoizers
 tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
@@ -142,79 +181,5 @@ def causal_mask(size):
 
 decoder_mask = causal_mask(seq_len)
 
-print(f"encoder_input: {encoder_input} \n encoder_mask: {encoder_mask} \n encoder_mask.shape: {encoder_mask.shape} \n decoder_mask: {decoder_mask}")
-
-
-
-
-
-
-
-
-train_ds = BilingualDataset(train_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
-val_ds = BilingualDataset(val_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
-
-# Find the maximum length of each sentence in the source and target sentence
-max_len_src = 0
-max_len_tgt = 0
-
-for item in ds_raw:
-    src_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
-    tgt_ids = tokenizer_tgt.encode(item['translation'][config['lang_tgt']]).ids
-    max_len_src = max(max_len_src, len(src_ids))
-    max_len_tgt = max(max_len_tgt, len(tgt_ids))
-
-print(f'Max length of source sentence: {max_len_src}')
-print(f'Max length of target sentence: {max_len_tgt}')
-
-
-train_dataloader = DataLoader(train_ds, batch_size=config['batch_size'], shuffle=True)
-val_dataloader = DataLoader(val_ds, batch_size=1, shuffle=True)
-
-
-
-
-
-
-
-
-
-
-
-
-def get_ds(config):
-    # It only has the train split, so we divide it overselves
-    ds_raw = load_dataset(f"{config['datasource']}", f"{config['lang_src']}-{config['lang_tgt']}", split='train')
-
-    # Build tokenizers
-    tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
-    tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, config['lang_tgt'])
-
-    # Keep 90% for training, 10% for validation
-    train_ds_size = int(0.9 * len(ds_raw))
-    val_ds_size = len(ds_raw) - train_ds_size
-    train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
-
-    train_ds = BilingualDataset(train_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
-    val_ds = BilingualDataset(val_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
-
-    # Find the maximum length of each sentence in the source and target sentence
-    max_len_src = 0
-    max_len_tgt = 0
-
-    for item in ds_raw:
-        src_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
-        tgt_ids = tokenizer_tgt.encode(item['translation'][config['lang_tgt']]).ids
-        max_len_src = max(max_len_src, len(src_ids))
-        max_len_tgt = max(max_len_tgt, len(tgt_ids))
-
-    print(f'Max length of source sentence: {max_len_src}')
-    print(f'Max length of target sentence: {max_len_tgt}')
-    
-
-    train_dataloader = DataLoader(train_ds, batch_size=config['batch_size'], shuffle=True)
-    val_dataloader = DataLoader(val_ds, batch_size=1, shuffle=True)
-
-    return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
-
-
+print(f" encoder_input: {encoder_input} \n encoder_mask: {encoder_mask} \n encoder_mask.shape: {encoder_mask.shape} \n decoder_mask: {decoder_mask}")
+""""
