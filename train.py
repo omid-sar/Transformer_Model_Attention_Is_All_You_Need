8fb7002ae1e491800e251a9946d427992505e671
omid-sar <mr.omid.sardari@gmail.com>
2024-01-23 19:53:52 -0500

    train_model_2

--- train.py
+++ train.py
@@ -1,13 +1,12 @@
 # Local Modules
 from dataset import BilingualDataset, causal_mask
 from model import built_transformer
-from config import get_config, get_weights_file_path
+from config import get_config, get_weights_file_path, latest_weights_file_path
 
 
 
 import torch
 import torch.nn as nn
-from torch.utils.data import random_split
 from torch.utils.data import DataLoader
 
 from torch.utils.tensorboard import SummaryWriter
@@ -179,29 +178,49 @@ def get_model(config, src_vocab_size, tgt_vocab_size):
     return model
 # ----------------------------------------------------------------------------------------------------   
 config = get_config()
-src_vocab_size, tgt_vocab_size)
+src_vocab_size = 30000
+tgt_vocab_size = 30000
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f" Using {device} device ")
 
 (Path(f"{config['datasource']}_{config['model_folder']}")).mkdir(parents=True, exist_ok=True)
 train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt = get_ds(config)
-model = get_model(config)
+model = get_model(config,src_vocab_size, tgt_vocab_size)
 # Tensorboard
 writer = SummaryWriter(config['experiment_name'])
 optimizer = torch.optim.Adam(model.parameters(), lr = config['lr'])
 
+initial_epoch = 0
+global_step = 0
+preload = config['preload']
 
+model_filename = latest_weights_file_path(config) if preload == 'latest' else get_weights_file_path(config, preload) if preload else None
 
+model_filename
 def train_model(config):
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f" Using {device} device ")
 
     (Path(f"{config['datasource']}_{config['model_folder']}")).mkdir(parents=True, exist_ok=True)
     train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt = get_ds(config)
-    model = get_model(config, )
+    model = get_model(config,src_vocab_size, tgt_vocab_size)
     # Tensorboard
     writer = SummaryWriter(config['experiment_name'])
     optimizer = torch.optim.Adam(model.parameters(), lr = config['lr'])
+
+    initial_epoch = 0
+    global_step = 0
+    preload = config['preload']
+    model_filename = latest_weights_file_path(config) if preload == 'latest' else get_weights_file_path(config, preload) if preload else None
+    if model_filename:
+        print(f'Preloading model {model_filename}')
+        state = torch.load(model_filename)
+        model.load_state_dict(state['model_state_dict'])
+        initial_epoch = state['epoch'] + 1
+        optimizer.load_state_dict(state['optimizer_state_dict'])
+        global_step = state['global_step']
+    else:
+        print('No model to preload, starting from scratch')
 
 
 
